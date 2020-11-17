3a5899eb577dd402f639b2b4d353da12b1ad8e17
hosam <hosamelmalt@gmail.com>
2020-11-17 10:59:02 +0200

    Initial Commit

---
+++ Fun Gane/Fun_GaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fun_GaneApp.swift
+//  Fun Gane
+//
+//  Created by hosam on 11/17/20.
+//
+
+import SwiftUI
+
+@main
+struct Fun_GaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
